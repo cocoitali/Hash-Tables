6319a621855ac99a57be27604c82cf7803bb7416
cocoitali <cmburatto@gmail.com>
2019-07-25 20:39:33 -0600

    b_hashtables.py : remove, retrieve, various updates

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -7,8 +7,7 @@ class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-
-
+        self.next = None
 # '''
 # Basic hash table
 # Fill this in.  All storage values should be initialized to None
@@ -17,8 +16,7 @@ class BasicHashTable:
     def __init__(self, capacity):
         self.capacity = capacity
         self.count = 0
-        self.elements = [None] * capacity
-
+        self.storage = [None] * capacity
 
 # '''
 # Fill this in.
@@ -36,11 +34,24 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    index = hash(key, hash_table.capacity )
-    print(index)
+    index = hash(key, hash_table.capacity)
+    
+    current_pair = hash_table.storage[index]
 
-    hash_table.elements[index] = value
+    last_pair = None
+    while current_pair is not None and current_pair.key != key: #if none, means we did not find a collision
+        last_pair = current_pair
+        current_pair = current_pair.next
 
+    if current_pair is None:
+        new_pair = Pair(key, value)
+        new_pair.next = hash_table.storage[index]
+        hash_table.storage[index]= new_pair
+
+
+    if hash_table.storage[index] is not None:
+        print("Warning: overwriting " + str(hash_table.elements[index].key))
+    hash_table.storage[index] = pair
 
 # '''
 # Fill this in.
@@ -48,20 +59,31 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
 
-
+    if hash_table.storage[index] is None:
+        print ("Warnign: removing non-existent key" + key)
+    has_table.storage[index] = None
+    
 # '''
 # Fill this in.
 
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
 
+    if hash_table.storage[index] is None:
+        print ("Warning: no value found for key " + key)
+        return None
+    return hash_table.storage[index]
 
 def Testing():
     ht = BasicHashTable(16)
+
+    hash_table_insert(ht, "a", "VALUE FOR A")
+    hash_table_insert(ht, "q", "VALUE FOR Q")
+    print(hash_table_retrieve(ht, "a"))
 
     hash_table_insert(ht, "line", "Here today...\n")
     print(ht.elements)
@@ -73,4 +95,4 @@ def Testing():
         print("ERROR:  STILL HERE")
 
 
-Testing()
+
